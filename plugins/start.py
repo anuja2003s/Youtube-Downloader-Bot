cc5c5f1229674627b5fec167e426e627a11052bc
Anuja Supulsara <88157026+anuja2003@users.noreply.github.com>
2021-11-18 20:25:50 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,14 +8,14 @@ async def start(client, message):
         [InlineKeyboardButton("➕ Add me to your Group ➕", url="https://t.me/youtube_Downloder_4k_bot?startgroup=true")],
         
         [
-            InlineKeyboardButton("MY😊 Group", url="https://t.me/anujasu"),
-            InlineKeyboardButton("MY😊 Channel", url="https://t.me/musicworldanu"),
+            InlineKeyboardButton("👥 Official Group", url="https://t.me/anujasu"),
+            InlineKeyboardButton("📣 Official Channel", url="https://t.me/musicworldanu"),
         
         ],
         [
             InlineKeyboardButton(
             "🎯 Owner", url="https://t.me/Anujasupulsara")]
     ])
-    welcomed = f"✨ Welcome <b>{message.from_user.first_name}</b>\n\n 🎯.allows you to youtube video Download📥 on groups through the new Telegram's Youtube link \n\n 🎯. /help 👈 For more information"
+    welcomed = f"✨ Welcome <b>{message.from_user.first_name}</b>\n\n 💭 ⭕️YouTube Downloader⭕️ allows you to youtube video Download📥 on groups through the new Telegram's Youtube link❕\n💡 All you have to do is send us the youtube link you want to download📥\n ❔Then select the quality of the Video or Song that you want to get \n\n 🎯. /help 👈 For more information"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
